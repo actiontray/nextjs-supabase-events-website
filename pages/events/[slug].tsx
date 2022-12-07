dad636ea943e6990701149ee5bc4acaf800761b8
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2022-12-07 10:57:28 +0100

    Defferentiate soft and hard constraints

--- pages/events/[slug].tsx
+++ pages/events/[slug].tsx
@@ -46,15 +46,27 @@ export default function Home({
       <main>
         <h1 className="text-center text-4xl py-4 px-2">{event.name}</h1>
         <p className="text-center text-xl py-4 px-2">{event.description}</p>
-        <p className="text-xl py-2 px-2">Possible timeframes:</p>
-        <p className="text-xl py-2 px-2">-</p>
-        <p className="text-xl py-2 px-2">Excluded timeframes:</p>
-        <p className="text-xl py-2 px-2">-</p>
-        <p className="text-xl py-2 px-2">Repeats:</p>
-        <p className="text-xl py-2 px-2">-</p>
-        <p className="text-xl py-2 px-2">Members - token:</p>
-        <p className="text-xl py-2 px-2">John Smith - firsttesttoken</p>
-        <p className="text-xl py-2 px-2">Eva Novak - secondtesttoken</p>
+        <p className="text-xl py-1 px-2">Duration:</p>
+        <p className="text-md py-1 px-2">30min</p>
+        <p className="text-xl py-1 px-2">Repeats:</p>
+        <p className="text-md py-1 px-2">Mon, Tue, Wed, Thu, Fri</p>
+        <h2 className="text-2xl pt-12 pb-4 px-2">Optimal (soft constraints)</h2>
+        <p className="text-xl py-1 px-2">Possible timeframes:</p>
+        <p className="text-md py-1 px-2">13:00-14:00</p>
+        <p className="text-xl py-1 px-2">Excluded timeframes:</p>
+        <p className="text-md py-1 px-2">-</p>
+        <p className="text-xl py-1 px-2">Members - token:</p>
+        <p className="text-md py-1 px-2">Eva Novak - secondtesttoken</p>
+        <p className="text-md py-1 px-2">John Smith - firsttesttoken</p>
+        <h2 className="text-2xl pt-12 pb-4 px-2">
+          Required (hard constraints)
+        </h2>
+        <p className="text-xl py-1 px-2">Possible timeframes:</p>
+        <p className="text-md py-1 px-2">12:00-15:00</p>
+        <p className="text-xl py-1 px-2">Excluded timeframes:</p>
+        <p className="text-md py-1 px-2">-</p>
+        <p className="text-xl py-1 px-2">Members - token:</p>
+        <p className="text-md py-1 px-2">Eva Novak - secondtesttoken</p>
       </main>
     </div>
   );
--- pages/events/new.tsx
+++ pages/events/new.tsx
@@ -9,29 +9,55 @@ export default function Home() {
         <h1 className="text-center text-4xl py-4 px-2">New event</h1>
 
         <form>
-          <div className="py-4 px-2">
+          <div className="py-2 px-2">
             <label htmlFor="name">Name</label>
             <input type="text" id="name" />
           </div>
-          <div className="py-4 px-2">
+          <div className="py-2 px-2">
             <label htmlFor="description">Description</label>
             <input type="text" id="description" />
           </div>
-          <div className="py-4 px-2">
-            <label htmlFor="possible-timeframes">Possible timeframes</label>
-            <input type="text" id="possible-timeframes" />
-          </div>
-          <div className="py-4 px-2">
-            <label htmlFor="excluded-timeframes">Excluded timeframes</label>
-            <input type="text" id="excluded-timeframes" />
-          </div>
-          <div className="py-4 px-2">
+          <div className="py-2 px-2">
             <label htmlFor="repeats">Repeats</label>
             <input type="text" id="repeats" />
           </div>
-          <div className="py-4 px-2">
-            <label htmlFor="members">Members</label>
-            <input type="text" id="members" />
+          <h2 className="text-2xl pt-8 pb-4 px-2">
+            Optimal (soft constraints)
+          </h2>
+          <div className="py-2 px-2">
+            <label htmlFor="optimal-possible-timeframes">
+              Possible timeframes
+            </label>
+            <input type="text" id="optimal-possible-timeframes" />
+          </div>
+          <div className="py-2 px-2">
+            <label htmlFor="optimal-excluded-timeframes">
+              Excluded timeframes
+            </label>
+            <input type="text" id="optimal-excluded-timeframes" />
+          </div>
+          <div className="py-2 px-2">
+            <label htmlFor="optimal-members">Members</label>
+            <input type="text" id="optimal-members" />
+          </div>
+          <h2 className="text-2xl pt-8 pb-4 px-2">
+            Required (hard constraints)
+          </h2>
+          <div className="py-2 px-2">
+            <label htmlFor="required-possible-timeframes">
+              Possible timeframes
+            </label>
+            <input type="text" id="required-possible-timeframes" />
+          </div>
+          <div className="py-2 px-2">
+            <label htmlFor="required-excluded-timeframes">
+              Excluded timeframes
+            </label>
+            <input type="text" id="required-excluded-timeframes" />
+          </div>
+          <div className="py-2 px-2">
+            <label htmlFor="required-members">Members</label>
+            <input type="text" id="required-members" />
           </div>
         </form>
       </main>
