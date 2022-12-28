4ee51254c6c801748d287dfe8f2067a4b346c4dc
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2022-12-29 00:27:33 +0100

    Update event details table

--- components/EventDetailsRow.tsx
+++ components/EventDetailsRow.tsx
@@ -8,11 +8,11 @@ export const EventDetailsRow: React.FC<EventDetailsRowProps> = ({
   value,
 }) => {
   return (
-    <tr>
-      <td className="text-right text-lg py-4 px-2 border border-slate-700">
+    <tr className="flex max-md:flex-wrap">
+      <td className="text-zinc-400 text-right max-md:text-left text-lg py-4 px-2 border border-zinc-200 dark:border-zinc-700 w-40 max-md:w-full">
         {name}
       </td>
-      <td className="text-left text-lg py-4 px-2 border border-slate-700">
+      <td className="text-left text-lg py-4 px-2 border border-zinc-200 dark:border-zinc-700 flex-auto max-md:w-full">
         {value}
       </td>
     </tr>
